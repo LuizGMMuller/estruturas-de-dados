4a39c6a8c27969d256b6a3a5c71cc48d7b95903b
Luiz G Muller <77251159+LuizGMMuller@users.noreply.github.com>
2025-02-26 00:36:51 +0000

    trab

--- exercise/doubly_linked_list.cpp
+++ exercise/doubly_linked_list.cpp
@@ -1,6 +1,11 @@
 #include "../include/doubly_linked_list.hpp"
 
+#include <gtest/gtest.h>
+
 int main(int argc, char const* argv[]) {
-  /* code */
+  TEST_F(DoublyLinkedListTest, TestPopFromEmptyList) {
+    EXPECT_THROW(list->pop_back(), std::out_of_range);
+    EXPECT_THROW(list->pop_front(), std::out_of_range);
+}
   return 0;
 }
